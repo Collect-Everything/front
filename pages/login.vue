30bd62ed18adf965c5a0b94f796f25f9854a5a5c
SpilmontFrancois <francois.spilmont5@etu.univ-lorraine.fr>
2023-05-30 15:04:37 +0200

    feat: finish login & register pages

--- pages/login.vue
+++ pages/login.vue
@@ -1,54 +1,22 @@
 <template>
-  <div
+  <Login
     v-if="page === 'login'"
-    class="flex flex-col items-center center-component w-full space-y-6"
-  >
-    <div class="flex flex-col items-center space-y-4 w-5/6">
-      <span class="text-xl font-bold">Se connecter</span>
-
-      <div class="flex flex-col space-y-1 text-gray-500 w-full">
-        <label for="email">{{ $t('login.email') }}</label>
-        <input id="email" v-model="email" type="email" />
-      </div>
-
-      <div class="flex flex-col space-y-1 text-gray-500 w-full">
-        <label for="password">{{ $t('login.password') }}</label>
-        <input id="password" v-model="password" type="password" />
-        <span class="text-xs text-black text-right" @click="forgotPassword()">
-          {{ $t('login.forgotPassword') }}
-        </span>
-      </div>
-    </div>
-
-    <button class="btn-primary w-5/6" @click="login()">
-      <fa-icon :icon="['fas', 'sign-in-alt']" />
-      <span>{{ $t('login.login') }}</span>
-    </button>
-
-    <div class="flex items-center space-x-2 w-5/6">
-      <div class="border-b w-full border-gray-500" />
-      <span class="text-gray-500">{{ $t('general.or') }}</span>
-      <div class="border-b w-full border-gray-500" />
-    </div>
-
-    <button class="btn-skeleton-primary w-5/6" @click="oauthLogin()">
-      <fa-icon :icon="['fab', 'google']" />
-      <span>{{ $t('login.oauthLogin') }}</span>
-    </button>
-
-    <div class="border-b w-5/6 border-gray-300" />
-
-    <div class="flex items-center space-x-1">
-      <span class="text-sm text-gray-500">{{ $t('login.noAccount') }}</span>
-      <span class="text-sm font-semibold" @click="page = 'register'">
-        {{ $t('login.register') }}
-      </span>
-    </div>
-  </div>
-  <div
+    v-model:email="email"
+    v-model:password="password"
+    @change-page="page = $event"
+  />
+  <Register
     v-else-if="page === 'register'"
-    class="flex flex-col items-center center-component w-full space-y-6"
-  ></div>
+    v-model:companyName="companyName"
+    v-model:email="email"
+    v-model:phone="phone"
+    v-model:address="address"
+    v-model:zipcode="zipcode"
+    v-model:city="city"
+    v-model:password="password"
+    v-model:passwordConfirm="passwordConfirm"
+    @change-page="page = $event"
+  />
 </template>
 
 <script lang="ts">
@@ -57,22 +25,14 @@ export default {
     return {
       email: '',
       password: '',
+      passwordConfirm: '',
+      companyName: '',
+      phone: '',
+      address: '',
+      zipcode: '',
+      city: '',
       page: 'login',
     }
-  },
-  methods: {
-    forgotPassword() {
-      // TODO
-    },
-    register() {
-      // TODO
-    },
-    login() {
-      // TODO
-    },
-    oauthLogin() {
-      // TODO
-    },
   },
 }
 </script>
