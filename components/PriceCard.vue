fb1ba8e0c7960f068005380f3b263ac58419158c
sakerlucas <sakerlucas@gmail.com>
2024-03-11 13:48:06 +0100

    feat: add prices page

---
+++ components/PriceCard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="flex flex-col justify-between rounded border border-green-600 bg-neutral-100 w-1/3"
+  >
+    <div class="flex flex-col p-4 w-full border-b border-gray-200 space-y-4">
+      <span class="text-green-600 text-2xl">{{ title }}</span>
+      <span>{{ subtitle }}</span>
+      <hr />
+      <div class="flex items-end space-x-2">
+        <span class="text-4xl">{{ price }} €</span>
+        <span class="text-sm">{{ isMonthly ? 'par mois' : 'par an' }}</span>
+      </div>
+    </div>
+    <div class="flex flex-col p-4 w-full space-y-4">
+      <span>Tous les avantages :</span>
+      <div class="flex flex-col space-y-2">
+        <div
+          v-for="advantage in advantages"
+          :key="advantage"
+          class="flex items-center space-x-2"
+        >
+          <fa-icon :icon="['fas', 'check']" class="text-green-600" />
+          <span>{{ advantage }}</span>
+        </div>
+      </div>
+      <button class="btn-secondary" @click="btnAction">
+        {{ btnText }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    advantages: {
+      type: Array<String>,
+      required: true,
+    },
+    btnText: {
+      type: String,
+      required: true,
+    },
+    btnAction: {
+      type: Function,
+      required: true,
+    },
+    isMonthly: {
+      type: Boolean,
+      required: true,
+    },
+  },
+}
+</script>
--- pages/prices/index.vue
+++ pages/prices/index.vue
@@ -1,3 +1,56 @@
 <template>
-  <div></div>
+  <div class="flex flex-col items-center space-y-6">
+    <div class="w-4/5 flex flex-col space-y-4">
+      <span class="text-2xl text-gray-700 text-center">Tarifs</span>
+      <span class="text-6xl text-center font-semibold title">
+        <!-- {{ $t('prices.subtitle') }} -->
+        Commencez gratuitement, puis payez seulement si ça vous plait
+      </span>
+      <span class="text-xl text-center">
+        Essayez Collect&Verything gratuitement pendant 14 jours
+      </span>
+    </div>
+    <div class="flex items-center space-x-2 justify-center">
+      <label class="switch">
+        <input id="switch" v-model="isMonthly" type="checkbox" />
+        <span class="slider round" />
+      </label>
+      <label for="switch" class="text-sm select-none">Tarifs mensuels</label>
+    </div>
+    <div class="flex space-x-4 justify-center w-full">
+      <PriceCard
+        v-for="subscription in subscriptions"
+        :key="subscription.title"
+        :title="subscription.title"
+        :subtitle="subscription.subtitle"
+        :price="isMonthly ? subscription.price : subscription.price * 12"
+        :advantages="subscription.advantages"
+        :btnText="subscription.btnText"
+        :btnAction="subscription.btnAction"
+        :isMonthly="isMonthly"
+      />
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      isMonthly: true,
+      subscriptions: [
+        {
+          title: 'Formule classique',
+          subtitle:
+            'Créez une boutique, ajoutez vos produits et encaissez les paiements de vos clients',
+          price: 30,
+          advantages: ['1 utilisateur', '1 projet'],
+          btnText: 'Essayer gratuitement',
+          btnAction: () => this.$router.push('/register'),
+        },
+      ],
+    }
+  },
+  methods: {},
+}
+</script>
