a74f50579fa4f787dd0b127e51419ad9ac22a1af
sakerlucas <sakerlucas@gmail.com>
2024-03-12 14:57:39 +0100

    feat: add about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <div></div>
+  <div class="flex flex-col items-center">
+    <div class="flex flex-col w-4/5 items-start space-y-6">
+      <div class="flex flex-col space-y-4">
+        <span class="lg:text-2xl text-gray-700">
+          {{ $t('about.about-us') }}
+        </span>
+        <span>
+          {{ $t('about.about-us-description') }}
+        </span>
+      </div>
+      <div class="flex flex-col space-y-4">
+        <span class="lg:text-2xl text-gray-700">
+          {{ $t('about.our-mission') }}
+        </span>
+        <span>
+          {{ $t('about.our-mission-description') }}
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
